63e694711fe1a1ad79d57595f816af35c7824c3c
Adam Dickinson <adamgamesyt@gmail.com>
2022-10-22 19:25:21 +0200

    Summary

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Routes
+
+from user import routes
+
 @app.route('/')
 def home():
-    return "Home"
+    return render_template('login.html')
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
---
+++ user/routes.py
@@ -0,0 +1,8 @@
+from crypt import methods
+from flask import Flask
+from app import app
+from user.models import User 
+
+@app.route('/user/signup', methods=['GET'])
+def signup():
+    return User().signup()
