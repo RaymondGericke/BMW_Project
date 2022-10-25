54891b3ecff0564aea5b593d762c0376e42b167c
Adam Dickinson <adamgamesyt@gmail.com>
2022-10-25 17:21:00 +0200

    s

--- user/routes.py
+++ user/routes.py
@@ -1,8 +1,7 @@
-from crypt import methods
 from flask import Flask
 from app import app
 from user.models import User 
 
-@app.route('/user/signup/', methods=['GET'])
+@app.route('/user/signup', methods=['GET'])
 def signup():
     return User().signup()
